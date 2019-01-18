b0b85581f578bc0ebc0c6ef155ef2a3d33ea9487
htruong1 <htruong1@ualberta.ca>
2019-01-18 15:39:30 -0500

    Modified TA's example to print out information

--- client.py
+++ client.py
@@ -3,19 +3,23 @@
 import socket
 
 HOST = "www.google.com"
-PORT = 80 #80 specifies the main HTTP port
+PORT = 80
 BUFFER_SIZE = 1024
 
+# payload = """GET / HTTP/1.0
+# Host: {}
+
+# """.format(HOST)
+
+# payload = f"GET / HTTP/1.0\r\nHost: {HOST}\r\n\r\n"
 payload = "GET / HTTP/1.0\r\nHost: {}\r\n\r\n".format(HOST)
 
-#Q1 sock_stream
-#put sockets in a try catch finally
 def get_request(addr):
     (family, socktype, proto, canonname, sockaddr) = addr
     try:
         s = socket.socket(family, socktype, proto)
-        s.connect(sockaddr) #now have to request something, want the home page
-        s.sendall(payload.encode()) #cant send info to sockets as string, need to change ot bytes
+        s.connect(sockaddr)
+        s.sendall(payload.encode())
         s.shutdown(socket.SHUT_WR)
 
         full_data = b""
@@ -23,9 +27,7 @@ def get_request(addr):
             data = s.recv(BUFFER_SIZE)
             if not data: break
             full_data += data
-
         print(full_data)
-
     except Exception as e:
         print(e)
     finally:
@@ -35,11 +37,8 @@ def main():
     addr_info = socket.getaddrinfo(HOST, PORT)
     for addr in addr_info:
         (family, socktype, proto, canonname, sockaddr) = addr
-        if(family == socket.AF_INET and socktype == 
-            socket.SOCK_STREAM):
-                print(addr)
-                get_request(addr)
-    pass
+        if family == socket.AF_INET and socktype == socket.SOCK_STREAM:
+            get_request(addr)
 
 if __name__ == "__main__":
     main()
